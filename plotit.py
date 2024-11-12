ebf9bb1d56c163ca3da10f2014a62be2e0b050d8
fbaulin <fbaulin@bmstu.ru>
2024-11-12 09:27:10 +0300

    Added another plotting backend

--- plotit.py
+++ plotit.py
@@ -1,8 +1,10 @@
+from typing import Iterable
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly.colors import qualitative
-from typing import Iterable
-import numpy as np
 
 # Make a line plot using plotly
 def plotit_legacy(y, x=None,
@@ -181,7 +183,7 @@ def plotit_legacy(y, x=None,
 
 
 
-def plotit(	y, x=None,
+def plotit_plotly(	y, x=None,
             title: str = None, legend=None,
             x_label=None, y_label=None,
             x_lims: list = None, y_lims: list = None,
@@ -294,3 +296,125 @@ def plotit(	y, x=None,
     if figure is not None:
         return None  # If figure is modified in place
     # return fig
+
+# Вывод графиков
+def plotit_pyplot(y, x=None,
+            names=None,
+            lines=None, widths=None, colors=None, opacities=None,
+            y_lims=None,   x_lims=None,
+            y_label=None, x_label=None,
+            y_scale=None,
+            title=None, grid=True,
+            ax=None,
+            ):
+    """Рисование графиков
+
+    Args:
+        y (_type_): ординаты
+        x (_type_, optional): абсциссы. Defaults to None.
+        names (list, optional): имена кривых для легенды. Defaults to None.
+        lines (list, optional): типы линий. Defaults to None.
+        widths (list, optional): толщины линий. Defaults to None.
+        colors (list, optional): цвета линий (целые значения - порядковые номера). Defaults to None.
+        opacities (list, optional): прозрачности. Defaults to None.
+        y_lims (list, optional): границы по ординатам. Defaults to None.
+        x_lims (list, optional): границы по абсциссам. Defaults to None.
+        y_label (str, optional): название оси ординат. Defaults to None.
+        x_label (str, optional): название оси абсцисс. Defaults to None.
+        y_scale (str, optional): тип оси (линейная, логарифм и проч.). Defaults to None.
+        title (str, optional): заголовок графика. Defaults to None.
+        grid (bool, optional): рисовать сетку. Defaults to True.
+        ax (_type_, optional): ось на которую добаляется график. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    if not isinstance(y,(list,tuple)): 
+        y = [y]
+        if isinstance(names,str): names = [names]
+    if names is None:  names=[None]*len(y)
+    if lines is None:  lines = ['solid'] * len(y)
+    if widths is None: widths = [1.5] * len(y)
+    if opacities is None:	  opacities = [1.0] * len(y)
+    if y_scale is None: y_scale='linear'
+    
+    if colors is None:
+        colors = [None] * len(y)
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    color_cycle = prop_cycle.by_key()['color']
+    
+    if not isinstance(x,(list,tuple)):
+        if x is None:   x = [np.arange(_.shape[-1]) for _ in y]
+        else:		   x = [x]*len(y)
+    if ax is None:		figure, ax = plt.subplots()
+    else: figure = None
+    for i in range(len(y)):
+        color = color_cycle[colors[i] % len(color_cycle)] if colors[i] is not None else None
+        ax.plot(x[i], y[i], label=names[i], color=color,
+                linestyle=lines[i], linewidth=widths[i], alpha=opacities[i])
+    # if names[0] is not None: ax.legend()
+    if names[0] is not None:
+        # ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+        ax.legend(loc='upper right', framealpha=0.5)  # framealpha sets transparency (0 = fully transparent, 1 = fully opaque)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_xlim(x_lims)
+    ax.set_ylim(y_lims)
+    ax.set_title(title)
+    ax.set_yscale(y_scale)
+    ax.grid(grid)
+    # if matplotlib.get_backend() == "Qt5Agg":
+    if 'Qt'.lower() in matplotlib.get_backend().lower():
+        ax.figure.canvas.manager.window.raise_()  # Only runs if Qt5Agg is used
+    # ax.figure.tight_layout()
+    if figure is not None: 	return figure, ax
+
+
+# Отображение созвездия
+def plot_constellation(complex_values, names=None, ax=None):
+    """
+    Plots a constellation diagram from an array of complex values.
+    Parameters:
+        complex_values (array-like): An array of complex numbers representing the signal points.
+    """
+    # Extract real and imaginary parts
+    real_parts = np.real(complex_values)
+    imaginary_parts = np.imag(complex_values)
+
+    # Create a scatter plot using matplotlib
+    if ax is None:
+        _, ax = plt.subplots()
+
+    scatter = ax.scatter(real_parts, imaginary_parts, s=8, label=names, alpha=0.75)
+
+    # Optional: label points with complex values
+    # for i, val in enumerate(complex_values):
+        # ax.text(real_parts[i], imaginary_parts[i], f'{val:.2f}', fontsize=10)
+
+    # Add axis labels and title
+    ax.set_title('Созвездие')
+    ax.set_xlabel('Действ.')
+    ax.set_ylabel('Мним.')
+    ax.grid(True)
+
+    lim = 1.1*np.max(np.abs(complex_values))
+    ax.set_xlim([-lim, lim])
+    ax.set_ylim([-lim, lim])
+
+    if names is not None:
+        ax.legend()
+
+    plt.gca().set_aspect('equal', adjustable='box')
+    
+    if names is not None:
+        ax.legend(loc='upper right', bbox_to_anchor=(1, 1))
+
+    # ax.figure.tight_layout()
+    # ax.figure.canvas.manager.window.raise_()
+    if ax is None:
+        plt.show()
+    elif isinstance(ax, list):
+        if len(ax) == 0:
+            ax.append(ax)
+        else:
+            ax[-1] = ax
