c1b26859b617e42124e549dd7a9b70dbb2322d89
fbaulin <fbaulin@bmstu.ru>
2024-11-13 19:25:14 +0300

    Updated methods signatures to match

--- plotit.py
+++ plotit.py
@@ -1,24 +1,37 @@
+""" Plotit module
+Модуль содержит отдельные функции для быстрой интеграции в произвольный код через 
+- копирование модуля в папку проекта или 
+- текста функции в текст пользовательской программы.
+
+"""
+
+# общие библиотеки
 from typing import Iterable
 import numpy as np
-import matplotlib
-from matplotlib import pyplot as plt
+# библиотеки для plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly.colors import qualitative
+# библиотеки для matplotlib
+import matplotlib
+from matplotlib import pyplot as plt
 
 
-
-def plotit_plotly(	y, x=None,
-            title: str = None, legend=None,
-            x_label=None, y_label=None,
-            x_lims: list = None, y_lims: list = None,
-            x_scale="linear", y_scale="linear",
-            error_x=None, error_y=None, error_thickness=1.0,
-            dashes=None, widths=None, opacity:Iterable = None, mode='lines',
-            colors=None,
-            renderer=None,
-            file_name=None,
-            figure=None, row_col=[None, None]):
+# Вывод графиков с использованием plotly
+def plotit_plotly(
+        y, x=None,
+        names=None,
+        lines=None, widths=None, colors=None, opacities=None,
+        x_label=None, y_label=None,
+        x_scale="linear", y_scale="linear",
+        x_lims: list = None,    y_lims: list = None,
+        title: str = None, 
+        ax=None, row_col=[None, None],
+        error_x=None, error_y=None, error_thickness=1.0,
+        mode='lines',
+        renderer=None,
+        file_name=None,
+        ):
     """
     Plot data using plotly.
     Args:
@@ -38,13 +51,26 @@ def plotit_plotly(	y, x=None,
         dashes (str, optional): List of line types. Valid values: 'solid' (=None), 'dash', 'dot', 'dashdot'.
         widths (optional): List of line widths, or a single value.
         opacity (Iterable, optional): List or value of opacity, transferred to the go.Scatter.
-        colors (optional): List of color names/values redirected to the go.Scatter object.
+        colors (optional): List of color names/values redirected to the go.Scatter object. Int values are supported.
         renderer (optional): Defines how to render the plot.
-        figure (optional): Returning figure for future use or modifying existing.
+        ax (optional): Returning ax for future use or modifying existing.
         row_col (list, optional): If subplots are used, select row and column.
         file_name (str, optional): Name of the file to export the plot image.
     Returns:
         plotly.graph_objects.Scatter: Returns Figure object, that can be altered.
+
+    Example 1. Plot single ndarray:
+        >>> import numpy as np
+        >>> from plotit import plotit_plotly as plot
+        >>> y = np.arange(10)**2
+        >>> plot(y)
+
+    Example 2. Plot list:
+        >>> import numpy as np
+        >>> from plotit import plotit_plotly as plot
+        >>> y = [np.arange(10)**2, np.arange(10)]
+        >>> plot(y, names=['quad', 'lin'])
+
     """
     # Handle 1D input by expanding to 2D
     if isinstance(y, np.ndarray) and y.ndim == 1:
@@ -66,9 +92,15 @@ def plotit_plotly(	y, x=None,
         y = np.real(y)
 
     n_plot_lines = min(y.shape[1], 10)  # Limit to 10 lines for clarity
-    if legend is None:
-        legend = [None] * n_plot_lines
-    line_properties = [{'dash': dashes[i] if dashes else None,
+    
+    # If colors are integers, select by order 
+    if colors is not None:
+        if isinstance(colors, (int, str)):  colors = [colors] * n_plot_lines
+        if isinstance(colors, list):        colors = [qualitative.D3[c] if isinstance(c, int) else c for c in colors]
+
+    if names is None:
+        names = [None] * n_plot_lines
+    line_properties = [{'dash': lines[i] if lines else None,
                         'width': widths[i] if widths else None,
                         'color': colors[i] if colors else None}
                        for i in range(n_plot_lines)]
@@ -76,28 +108,28 @@ def plotit_plotly(	y, x=None,
         x = np.arange(y.shape[0])
     plots = [
         go.Scatter(
-            x=x, y=y[:, i], mode=mode, name=legend[i],
+            x=x, y=y[:, i], mode=mode, name=names[i],
             line=line_properties[i],
-            opacity=opacity[i] if opacity else None,
+            opacity=opacities[i] if opacities else None,
             error_x=dict(type='data', array=error_x[:, i], visible=True, thickness=error_thickness) if isinstance(error_x, np.ndarray) else None,
             error_y=dict(type='data', array=error_y[:, i], visible=True, thickness=error_thickness) if isinstance(error_y, np.ndarray) else None
         )
         for i in range(n_plot_lines)
     ]
-    if figure is None:
+    if ax is None:
         fig = go.Figure(layout=go.Layout(showlegend=True, title=title))
-    elif isinstance(figure, list):
-        if len(figure)==0:
-            figure.append(go.Figure(layout=go.Layout(title=title)))
-        elif figure[-1] is None:
-            figure[-1] = go.Figure(layout=go.Layout(title=title))
-        elif isinstance(figure[-1], int):
-            f = figure[-1]
+    elif isinstance(ax, list):
+        if len(ax)==0:
+            ax.append(go.Figure(layout=go.Layout(title=title)))
+        elif ax[-1] is None:
+            ax[-1] = go.Figure(layout=go.Layout(title=title))
+        elif isinstance(ax[-1], int):
+            f = ax[-1]
             f = (f,1) if f<10 else divmod(f)
-            figure[-1] = make_subplots(*f, shared_xaxes='columns')
-        fig = figure[-1]
+            ax[-1] = make_subplots(*f, shared_xaxes='columns')
+        fig = ax[-1]
     else:
-        fig = figure
+        fig = ax
 
     if isinstance(row_col, int):
         if row_col<=10:
@@ -109,7 +141,7 @@ def plotit_plotly(	y, x=None,
     fig.update_xaxes(title_text=x_label, range=x_lims, type=x_scale, tickformat='s')
     fig.update_yaxes(title_text=y_label, range=y_lims, type=y_scale, tickformat='s')
     if renderer is None:
-        if figure is None:
+        if ax is None:
             fig.show()
     elif renderer:
         if isinstance(renderer, str):
@@ -118,20 +150,23 @@ def plotit_plotly(	y, x=None,
             fig.show(renderer='browser')
     if file_name:
         fig.write_image(file_name)
-    if figure is not None:
+    if ax is not None:
         return None  # If figure is modified in place
     # return fig
 
-# Вывод графиков
-def plotit_pyplot(y, x=None,
-            names=None,
-            lines=None, widths=None, colors=None, opacities=None,
-            y_lims=None,   x_lims=None,
-            y_label=None, x_label=None,
-            y_scale=None,
-            title=None, grid=True,
-            ax=None,
-            ):
+
+# Вывод графиков с использованием matplotlib
+def plotit_pyplot(
+        y, x=None,
+        names=None,
+        lines=None, widths=None, colors=None, opacities=None,
+        x_label=None,   y_label=None,
+        x_scale='linear',   y_scale='linear',
+        x_lims: list = None, y_lims: list = None,
+        title: str = None,  
+        ax=None,
+        grid=True,
+        ):
     """Рисование графиков
 
     Args:
@@ -142,26 +177,38 @@ def plotit_pyplot(y, x=None,
         widths (list, optional): толщины линий. Defaults to None.
         colors (list, optional): цвета линий (целые значения - порядковые номера). Defaults to None.
         opacities (list, optional): прозрачности. Defaults to None.
+        y_scale (str, optional): тип оси (линейная, логарифм и проч.). Defaults to None.
         y_lims (list, optional): границы по ординатам. Defaults to None.
         x_lims (list, optional): границы по абсциссам. Defaults to None.
         y_label (str, optional): название оси ординат. Defaults to None.
         x_label (str, optional): название оси абсцисс. Defaults to None.
-        y_scale (str, optional): тип оси (линейная, логарифм и проч.). Defaults to None.
         title (str, optional): заголовок графика. Defaults to None.
         grid (bool, optional): рисовать сетку. Defaults to True.
         ax (_type_, optional): ось на которую добаляется график. Defaults to None.
 
     Returns:
         _type_: _description_
+
+
+    Example 1. Plot single ndarray:
+        >>> import numpy as np
+        >>> from plotit import plotit_pyplot as plot
+        >>> y = np.arange(10)**2
+        >>> plot(y)
+
+    Example 2. Plot list:
+        >>> import numpy as np
+        >>> from plotit import plotit_pyplot as plot
+        >>> y = [np.arange(10)**2, np.arange(10)]
+        >>> plot(y, names=['quad', 'lin'])
     """
     if not isinstance(y,(list,tuple)): 
         y = [y]
         if isinstance(names,str): names = [names]
-    if names is None:  names=[None]*len(y)
-    if lines is None:  lines = ['solid'] * len(y)
-    if widths is None: widths = [1.5] * len(y)
-    if opacities is None:	  opacities = [1.0] * len(y)
-    if y_scale is None: y_scale='linear'
+    if names is None:       names=[None]*len(y)
+    if lines is None:       lines = ['solid'] * len(y)
+    if widths is None:      widths = [1.5] * len(y)
+    if opacities is None:	opacities = [1.0] * len(y)
     
     if colors is None:
         colors = [None] * len(y)
@@ -186,6 +233,7 @@ def plotit_pyplot(y, x=None,
     ax.set_xlim(x_lims)
     ax.set_ylim(y_lims)
     ax.set_title(title)
+    ax.set_xscale(x_scale)
     ax.set_yscale(y_scale)
     ax.grid(grid)
     # if matplotlib.get_backend() == "Qt5Agg":
